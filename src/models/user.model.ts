a781c32b78a683b559eae87dddd784cf3c65a573
ItzVakko <itzvakkoofficial@gmail.com>
2025-05-03 15:41:12 +0400

    added user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser extends Document {
+  email: string;
+  fullName: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  fullName: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
