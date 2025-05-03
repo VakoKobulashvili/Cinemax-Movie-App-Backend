9e875d9c3f3444b664b58d37243afc11def472ef
ItzVakko <itzvakkoofficial@gmail.com>
2025-05-03 15:08:33 +0400

    set up index.ts fully and wrote auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
-import { MongooseError } from "mongoose";
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose, { MongooseError } from "mongoose";
+import cors from "cors";
 
 const app = express();
+const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
 
 mongoose
   .connect("mongodb://localhost:27017/cinemax-authentication", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  } as mongoose.ConnectOptions)
   .then(() => console.log("Mongoose connected successfully!"))
   .catch((err: MongooseError) =>
     console.log("Could not connect mongoose: ", err)
   );
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("App started!");
 });
