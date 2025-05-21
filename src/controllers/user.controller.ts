fb439bbe74622e74b49b25e2fa172cfeead74bd5
ItzVakko <itzvakkoofficial@gmail.com>
2025-05-21 16:22:33 +0400

    added user update controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -29,3 +29,31 @@ export const getUser = async (
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const updateUser = async (req: AuthRequest, res: Response) => {
+  try {
+    const { fullName, email, password, avatar } = req.body;
+    const user = await User.findOne({ _id: req.user._id });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (fullName) user.fullName = fullName;
+    if (email) user.email = email.toLowerCase();
+    if (password) user.password = password;
+    if (avatar) user.avatar = avatar;
+
+    await user.save();
+
+    res.json({
+      message: "User updated successfully!",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        avatar: user.avatar,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update user!", error });
+  }
+};
